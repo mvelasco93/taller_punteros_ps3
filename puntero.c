11a92859906793d32b88b0bb496c33824c305e99
Edwin Boza <eboza@espol.edu.ec>
2017-10-30 12:24:03 -0500

    Create puntero.c

---
+++ puntero.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void)
+{
+        int variable = 10;
+        int *puntero = &variable;
+        *puntero = 10;
+        printf("&variable : %p\n", &variable);
+        printf("puntero   : %p\n", puntero);
+        printf("&puntero  : %p\n", &puntero);
+        printf("variable  : %d\n", variable);
+        printf("*puntero  : %d\n", *puntero);
+        printf("Tamanio de puntero  : %d bits\n", sizeof(puntero)*8);
+        return 0;
+}
